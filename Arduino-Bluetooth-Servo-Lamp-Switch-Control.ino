45abea3ebaebe6cee4788928d73f4ae076412e12
Ahmet Akif Kaya <ahmetakifkaya11@gmail.com>
2017-08-09 09:06:26 +0300

    Add files via upload

---
+++ Arduino-Bluetooth-Servo-Lamp-Switch-Control.ino
@@ -0,0 +1,51 @@
+#include <Servo.h>
+
+Servo steer;
+
+const int buzzer = 11;
+String readString;
+
+int val1 = 120;
+int val2 = 50;
+int val3 = 88;
+
+void setup() 
+{
+  Serial.begin(9600); 
+  pinMode(buzzer , OUTPUT);
+  steer.attach(3);   
+}
+void loop() 
+{
+  while (Serial.available()) 
+  {
+    delay(3);  
+    char c = Serial.read();
+    readString += c; 
+  }
+  if (readString.length() >0) 
+  {
+    Serial.println(readString);
+    if (readString == "k")
+    {
+      steer.write(val1);
+    }
+    else if (readString == "a")
+    {
+      steer.write(val2);
+    }
+    else
+    {
+      steer.write(val3);
+    }
+    if (readString == "x")
+    {
+      digitalWrite(buzzer , 1);
+    }
+    else
+    {
+      digitalWrite(buzzer , 0);
+    }
+    readString="";
+  } 
+}
